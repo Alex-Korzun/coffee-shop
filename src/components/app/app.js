32c6faa892e4a8d35200482d2ea1775cd7c38288
Alex Korzun <koleksandr4@gmail.com>
2024-01-25 17:09:51 +0200

    first changes

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import MainPage from '../main-page/main-page';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main-page/main-page.js
@@ -0,0 +1,15 @@
+import './main-page.css';
+
+const MainPage = () => {
+    return (
+        <div className='main-page' >
+            <header>
+                <button className='btn'>Coffee house</button>
+                <button className='btn'>Our coffee</button>
+                <button className='btn'>For your pleasure</button>
+            </header>
+        </div>
+    );
+}
+
+export default MainPage;
